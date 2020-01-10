590ef816fafc33b9605b3624661ef377b01a38ed
juanmanual <juan.melendez3.14@gmail.com>
2020-01-09 20:05:29 -0500

    add array example to main

--- src/com/github/juanmanual/Main.java
+++ src/com/github/juanmanual/Main.java
@@ -1,7 +1,32 @@
 package com.github.juanmanual;
 
+import java.util.ArrayList;
+
 public class Main{
     public static void main(String args[]) {
         System.out.println("Hello world");
+        // golf
+        // int[] golfScores = new int[18];
+
+        // int[][] golfGames = new int[3][18];
+
+        // for(int i = 0; i< golfScores.length; i++) {
+        //     golfScores[i] = -1;
+        // }
+
+        // for(int i = 0; i< golfScores.length; i++) {
+        //     System.out.println(golfScores[i]);
+        // }
+
+        // for(int i = 0; i<golfGames.length; i++) {
+        //     int[] golfGame = golfGames[i];
+        //     for(int j = 0; j< golfScores.length; j++) {
+        //         golfGame[j] = -1;
+        //     }
+        // }
+
+        ArrayList<Integer> golfScores = new ArrayList<Integer>();
+
+        golfScores.add(-1);
     }
 }
